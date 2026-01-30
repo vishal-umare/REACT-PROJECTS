faa1397ca16e79fb96c7e8c0ecf387a7c2b1f1f0
vishalumare <vishaldemo2004@gmail.com>
2026-01-30 23:24:43 +0530

    Created the Pricing Section

--- MODERN_UI_UX/src/components/Pricing.jsx
+++ MODERN_UI_UX/src/components/Pricing.jsx
@@ -1,4 +1,4 @@
-import { Star } from "lucide-react";
+import { Check, Play, Star } from "lucide-react";
 import React from "react";
 
 const plans = [
@@ -80,31 +80,75 @@ function Pricing() {
           {plans.map((plan, key) => (
             <div
               key={key}
-              className={`relative bg-slate-900/50 backdrop-blur-sm border rounded-xl sm:rounded-2xl p-6 sm:p-8 transition-all duration-300 overflow-visible group flex flex-cols h-full ${
-                plan.mostPopular 
-                ? "border-blue-500 shadow-2xl shadow-blue-500/20 lg:scale-105" 
-                : "border-slate-800 hover:border-slate-700 "
-            }`}
+              className={`relative bg-slate-900/50 backdrop-blur-sm border rounded-xl sm:rounded-2xl p-6 sm:p-8 transition-all duration-300 overflow-visible group flex flex-col h-full ${
+                plan.mostPopular
+                  ? "border-blue-500 shadow-2xl shadow-blue-500/20 lg:scale-105"
+                  : "border-slate-800 hover:border-slate-700 "
+              }`}
             >
-                {/* most popular tag */}
-                {plan.mostPopular && (
-                    <div className="absolute -top-2 sm:-top-3 left-1/2 trasnform -translate-x-1/2  z-10 ">
-                        <div className="flex items-center space-x-1 px-3 sm:px-4 py-1 sm:py-1.5 bg-gradient-to-b from-blue-500 to-cyan-500 rounded-full text-xs sm:text-sm font-semibold shadow-lg ">
-                            <Star className="w-3 h-3 sm:w-4 sm:h-4 fill-white "/>
-                            <span>Most Popular</span>
-                        </div>
-                    </div>
-                )}
-                <div className="text-center mb-6 sm:mb-8 ">
-                    <h3 className="text-xl sm:text-2xl font-bold mb-2 ">{plan.name}</h3>
-                    <p>{plan.description}</p>
-                    <div>
-                        <span>${plan.price}</span>
-                        <span>/month</span>
-                    </div>
+              {/* Glassy effect */}
+              <div className="absolute inset-0 bg-gradient-to-br from-white/10 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 transform -translate-x-full -translate-y-full group-hover:translate-x-0 group-hover:translate-y-0 pointer-events-none rounded-lg" />
+              {/* most popular tag */}
+              {plan.mostPopular && (
+                <div className="absolute -top-2 sm:-top-3 left-1/2 trasnform -translate-x-1/2  z-10 ">
+                  <div className="flex items-center space-x-1 px-3 sm:px-4 py-1 sm:py-1.5 bg-gradient-to-b from-blue-500 to-cyan-500 rounded-full text-xs sm:text-sm font-semibold shadow-lg ">
+                    <Star className="w-3 h-3 h-3 sm:w-3 sm:h-3 fill-white " />
+                    <span>Most Popular</span>
+                  </div>
                 </div>
+              )}
+              <div className="text-center mb-6 sm:mb-8 ">
+                <h3 className="text-xl sm:text-2xl font-bold mb-2 ">
+                  {plan.name}
+                </h3>
+                <p className="text-gray-400 text-xs sm:text-sm mb-3 sm:mb-4">
+                  {plan.description}
+                </p>
+                <div className="flex items-baseline justify-center ">
+                  <span className="text-3xl sm:text-4xl lg:tex-5xl font-bold bg-gradient-to-b from-blue-400 to-cyan-400 bg-clip-text text-transparent">
+                    ${plan.price}
+                  </span>
+                  <span className="text-gray-300 ml-1 sm:ml-2 text-sm sm:text-base ">
+                    /month
+                  </span>
+                </div>
+              </div>
+
+              <ul className="space-y-3 sm:space-y-4 mb-6 sm:mb-8 flex-row ">
+                {plan.features.map((feature, featureKey) => (
+                  <li
+                    key={featureKey}
+                    className="flex items-start space-x-2 sm:space-x-3 "
+                  >
+                    <div className="flex-shrink-0 w-4 h-4 sm:h-5 sm:w-5 rounded-full bg-blue-500/20 flex items-center justify-center mt-0.5 ">
+                      <Check className="w-2.5 h-2.5 sm:h-3 sm:w-3 text-blue-400 " />
+                    </div>
+                    <span className="text-gray-300 text-sm sm:text-base ">
+                      {feature}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+              <button
+                className={`w-full py-2.5 sm:py-3 px-4 sm:px-6 rounded-lg font-semibold transition-all duration-300 mt-auto hover:scale-102 cursor-pointer text-sm sm:text-base ${
+                  plan.mostPopular
+                    ? "bg-gradient-to-b from-blue-500 to-cyan-500"
+                    : "bg-white/5 border border-white/10 hover:bg-white/10"
+                }`}
+              >
+                Get Started
+              </button>
             </div>
           ))}
+        </div>
+
+        <div className="mt-8 sm:mt-12 text-center">
+          <p className="text-gray-400 text-base text-xl">
+            Need a custom plan?{" "}
+            <a href="#" className="text-blue-400 hover:text-blue-300">
+              Contact our sales team
+            </a>
+          </p>
         </div>
       </div>
     </section>
